6fa4115b9dca7fc44093bbca17db91cdb00a5a37
Marcela Ceneviva Auslenter <cotchoninha@yahoo.com.br>
2018-04-16 23:04:53 +0100

    can access the library

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imagePickerView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +21,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func choseFromLibrary(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        present(pickerController, animated: true, completion: nil)
+    }
+    
 
 
 }
