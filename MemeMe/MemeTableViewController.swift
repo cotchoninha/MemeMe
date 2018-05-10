41d7d556bd858fde3399035c65f614ea907d4cf7
Marcela Ceneviva Auslenter <cotchoninha@yahoo.com.br>
2018-05-10 18:29:52 +0100

    worked with the constraints on table view and collection

--- MemeMe/MemeTableViewController.swift
+++ MemeMe/MemeTableViewController.swift
@@ -11,23 +11,26 @@ import UIKit
 class MemeTableViewController: UITableViewController{
     
     var memes: [Meme]! {
-//        let object = UIApplication.shared.delegate
-//        let appDelegate = object as! AppDelegate
-//        return appDelegate.memes
-//        
-        let meme1 = Meme(topText: "dabhfa lk jdsf nlakjsdnf lkas djnfa sdjfnasdfna", bottomText: "kdhfs sdjfhaslkdj sdjfhasdkj jsdfsad dfjd djfdnsalk", originalImage: #imageLiteral(resourceName: "me"), memedImage: #imageLiteral(resourceName: "me"))
-        let meme2 = Meme(topText: "2", bottomText: "2", originalImage: #imageLiteral(resourceName: "me"), memedImage: #imageLiteral(resourceName: "me"))
-        let meme3 = Meme(topText: "3", bottomText: "3", originalImage: #imageLiteral(resourceName: "me"), memedImage: #imageLiteral(resourceName: "me"))
-        return [meme1, meme2, meme3]
+        let object = UIApplication.shared.delegate
+        let appDelegate = object as! AppDelegate
+        return appDelegate.memes
+//        let meme1 = Meme(topText: "dabhfa lk jdsf nlakjsdnf lkas djnfa sdjfnasdfna", bottomText: "kdhfs sdjfhaslkdj sdjfhasdkj jsdfsad dfjd djfdnsalk", originalImage: #imageLiteral(resourceName: "me"), memedImage: #imageLiteral(resourceName: "me"))
+//        let meme2 = Meme(topText: "2", bottomText: "2", originalImage: #imageLiteral(resourceName: "me"), memedImage: #imageLiteral(resourceName: "me"))
+//        let meme3 = Meme(topText: "3", bottomText: "3", originalImage: #imageLiteral(resourceName: "me"), memedImage: #imageLiteral(resourceName: "me"))
+//        return [meme1, meme2, meme3]
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return memes.count
     }
     
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "tableViewCell", for: indexPath) as! MemeTableViewCell
-        let memeSquare = memes[indexPath.row]
+        let memeSquare = self.memes[indexPath.row]
         
         cell.memeCollectionImage.image = memeSquare.memedImage
         cell.memeDetailText.text = memeSquare.topText + memeSquare.bottomText
