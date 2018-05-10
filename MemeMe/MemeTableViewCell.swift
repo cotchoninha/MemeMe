c0c1e9e74fde1e665300327fcc1bc0f5cbc6e55f
Marcela Ceneviva Auslenter <cotchoninha@yahoo.com.br>
2018-05-10 12:50:56 +0100

    implemented table view + constraints

---
+++ MemeMe/MemeTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MemeTableViewCell.swift
+//  MemeMe
+//
+//  Created by Marcela Ceneviva Auslenter on 09/05/2018.
+//  Copyright © 2018 Marcela Ceneviva Auslenter. All rights reserved.
+//
+
+import UIKit
+
+class MemeTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var memeCollectionImage: UIImageView!
+    @IBOutlet weak var memeDetailText: UILabel!
+
+    
+}
--- MemeMe/MemeTableViewController.swift
+++ MemeMe/MemeTableViewController.swift
@@ -11,9 +11,14 @@ import UIKit
 class MemeTableViewController: UITableViewController{
     
     var memes: [Meme]! {
-        let object = UIApplication.shared.delegate
-        let appDelegate = object as! AppDelegate
-        return appDelegate.memes
+//        let object = UIApplication.shared.delegate
+//        let appDelegate = object as! AppDelegate
+//        return appDelegate.memes
+//        
+        let meme1 = Meme(topText: "dabhfa lk jdsf nlakjsdnf lkas djnfa sdjfnasdfna", bottomText: "kdhfs sdjfhaslkdj sdjfhasdkj jsdfsad dfjd djfdnsalk", originalImage: #imageLiteral(resourceName: "me"), memedImage: #imageLiteral(resourceName: "me"))
+        let meme2 = Meme(topText: "2", bottomText: "2", originalImage: #imageLiteral(resourceName: "me"), memedImage: #imageLiteral(resourceName: "me"))
+        let meme3 = Meme(topText: "3", bottomText: "3", originalImage: #imageLiteral(resourceName: "me"), memedImage: #imageLiteral(resourceName: "me"))
+        return [meme1, meme2, meme3]
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -21,10 +26,11 @@ class MemeTableViewController: UITableViewController{
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "tableViewCell")!
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tableViewCell", for: indexPath) as! MemeTableViewCell
         let memeSquare = memes[indexPath.row]
         
-        cell.imageView?.image = memeSquare.memedImage
+        cell.memeCollectionImage.image = memeSquare.memedImage
+        cell.memeDetailText.text = memeSquare.topText + memeSquare.bottomText
         
         return cell
     }
